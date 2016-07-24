a820ac1aff920c60667b5b2ef4508b0502a61e3e
Tim Davis <tcd@umn.edu>
2016-07-24 01:12:40 -0500

    trying to fix issues with QT5 and keyboard added environment variable XKB_DEFAULT_LAYOUT=us

--- mythtv/src/004-bring-up-rdp.sh
+++ mythtv/src/004-bring-up-rdp.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+export XKB_DEFAULT_RULES=evdev
+export XKB_DEFAULT_MODEL=pc105
+export XKB_DEFAULT_LAYOUT=us
+#export XKB_DEFAULT_VARIANT=
+#export XKB_DEFAULT_OPTIONS=
 mkdir -p /home/mythtv/
 mkdir -p  /var/run/sshd
 mkdir  -p /root/.vnc
